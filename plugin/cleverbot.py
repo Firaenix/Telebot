c1c983e9f81d53609e8c96e5861b1d80df64273b
Hexane <zenhexane@gmail.com>
2014-12-02 10:19:15 -0600

    Plugins.

---
+++ plugin/cleverbot.py
@@ -0,0 +1,21 @@
+###########################################################################
+# CleverBot Plugin							  #
+# Hexane								  #
+###########################################################################
+from libraries import cleverbot
+
+def help():
+        return "!cb [message]: Returns response from cleverbot"
+
+def do(msg):
+	cb = cleverbot.Cleverbot()	
+        return cb.ask(msg)
+
+def getCmd():
+        return "!cb"
+
+def getArgs():
+        return 1
+
+def hasEncodings():
+	return True
